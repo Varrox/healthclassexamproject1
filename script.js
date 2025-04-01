5871e4b15fa6eba210ecfd5f91979b88f567372d
Varrox <willkat320@outlook.com>
2025-04-01 13:17:44 -0400

    m

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculateTsp(grams)
+{
+    return 12.5 / grams;
+}
+
+function calculateWater(weight, kilograms)
+{
+    return weight * (kilograms ? 2.205 : 1) / 2;
+}
+
+function calculateCalories(age, weight, kilograms, heightInInches, gender) // gender is true for male and false for female
+{
+    return 655 + (gender ? 9.6 : 4.3) * weight * (kilograms ? 2.205 : 1) + (gender ? 1.85 : 4.7) * heightInInches - 4.7 * age
+}
